c6db7fda0cbdd0d61727b477414992468587bf7b
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2023-05-06 14:24:55 +0200

    remove redundant imports

--- szkala/mapreduce/steam_01_combine/src/test/scala/SteamCombineTest.scala
+++ szkala/mapreduce/steam_01_combine/src/test/scala/SteamCombineTest.scala
@@ -1,7 +1,3 @@
-import JoinSteamDatasets.SteamStoreOrSpyMapper
-import org.junit.runner.RunWith
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.PowerMockRunner
 import org.scalatest.funsuite.AnyFunSuite
 
 class SteamCombineTest extends AnyFunSuite:
