bad56d3525e0a84cbb3095efc968a7f3c6676f8d
michalliss <michal.j.liss@gmail.com>
2023-05-07 20:05:14 +0200

    sprawko aaaa

--- szkala/mapreduce/steam_02_choose/src/main/scala/Main.scala
+++ szkala/mapreduce/steam_02_choose/src/main/scala/Main.scala
@@ -21,13 +21,13 @@ import java.time.temporal.TemporalField
 import scala.util.{Failure, Success, Try}
 
 case class SteamJoined(
-  game_id: Int,
-  name: String,
-  positive: Int,
-  negative: Int,
-  owners: String,
-  ccu: Int,
-  release_date: String
+    game_id: Int,
+    name: String,
+    positive: Int,
+    negative: Int,
+    owners: String,
+    ccu: Int,
+    release_date: String
 )
 
 object SteamJoined {
@@ -39,34 +39,32 @@ object Main {
   private val isoFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
   class MyMapper extends HadoopJob.HadoopMapper[AnyRef, Text, Text, Text] {
     override def myMap(key: AnyRef, value: Text, emit: (Text, Text) => Unit): Unit = {
-
-      value.toString.split("\n").map(x => x.dropWhile(!_.isWhitespace)).toList
+      value.toString
+        .split("\n")
+        .map(x => x.dropWhile(!_.isWhitespace))
+        .toList
         .flatMap(x => SteamJoined.decoder.decodeJson(x).toOption)
         .filter(game => before2020(game.release_date))
         .filter(game => overwhelminglyPositive(game))
         .filter(game => game.ccu > 5000)
         .filter(game => has_many_owners(game))
         .foreach(x => emit(Text(x.game_id.toString), Text(x.toJson)))
-
-
-/*      value.toString.split("\n").map(x => x.dropWhile(!_.isWhitespace)).toList
-        .flatMap(x => SteamJoined.decoder.decodeJson(x).toOption)
-        .filter(game => before2020(game.release_date))
-        .filter(game => game.positive > 30000)
-        .filter(game => (game.positive.toDouble / (game.positive + game.negative)) > 0.9)
-        .foreach(x => emit(Text("id"), Text(x.toJson)))*/
     }
 
-    private def overwhelminglyPositive(steamJoined: SteamJoined): Boolean = steamJoined.positive > 30000 && (steamJoined.positive.toDouble / (steamJoined.positive + steamJoined.negative)) > 0.8
+    private def overwhelminglyPositive(steamJoined: SteamJoined): Boolean =
+      steamJoined.positive > 30000 &&
+        (steamJoined.positive.toDouble / (steamJoined.positive + steamJoined.negative)) > 0.8
+
     private def before2020(date: String): Boolean = {
       Try(isoFormat.parse(date)) match
-        case Failure(exception) => false
+        case Failure(_)     => false
         case Success(value) => value.get(java.time.temporal.ChronoField.YEAR) < 2020
     }
-//10,000,000 .. 20,000,000
-    private def has_many_owners(steamJoined: SteamJoined): Boolean = Try(steamJoined.owners.replace(",", "").split('.').head.trim.toInt) match
-      case Failure(exception) => false
-      case Success(value) => value > 5000000
+
+    private def has_many_owners(steamJoined: SteamJoined): Boolean =
+      Try(steamJoined.owners.replace(",", "").split('.').head.trim.toInt) match
+        case Failure(_)     => false
+        case Success(value) => value > 5000000
   }
 
   class MyReducer extends HadoopJob.HadoopReducer[Text, Text, Text] {
@@ -77,7 +75,7 @@ object Main {
 
   def main(args: Array[String]) = {
     val conf = new Configuration
-    val job = Job.getInstance(conf, "Select games for further analysis")
+    val job  = Job.getInstance(conf, "Select games for further analysis")
 
     job.setJarByClass(classOf[Main.type])
     job.setMapperClass(classOf[MyMapper])
