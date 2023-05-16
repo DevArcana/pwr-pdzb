46dd220ee43246020694052b800b03b6beabfb3b
michalliss <michal.j.liss@gmail.com>
2023-05-16 11:20:13 +0200

    working dockerfile!

--- vagrant2/hadoop/start_all.sh
+++ vagrant2/hadoop/start_all.sh
@@ -1,11 +1,11 @@
 docker compose up -d
 
-docker exec -u 0 namenode bash -c "echo 'root:pass' | chpasswd"
-docker exec -u 0 namenode bash -c "apt-get -y update"
-docker exec -u 0 namenode bash -c "apt-get -y install openssh-client openssh-server"
-docker exec -u 0 namenode bash -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
-docker exec -u 0 namenode bash -c "service ssh restart"
-docker exec -u 0 namenode bash -c "export -p >env_var.sh"
+# docker exec -u 0 namenode bash -c "echo 'root:pass' | chpasswd"
+# docker exec -u 0 namenode bash -c "apt-get -y update"
+# docker exec -u 0 namenode bash -c "apt-get -y install openssh-client openssh-server"
+# docker exec -u 0 namenode bash -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
+# docker exec -u 0 namenode bash -c "service ssh restart"
+# docker exec -u 0 namenode bash -c "export -p >env_var.sh"
 
 #docker exec -u 0 jupyter bash -c "sudo gem install apt-spy2"
 #docker exec -u 0 jupyter bash -c "sudo apt-spy2 fix --commit --launchpad --country=PL"
---
+++ vagrant2/sprawozdania/.bash_history
@@ -0,0 +1,2 @@
+ssh root@namenode
+exit
