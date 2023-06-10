41dbdd4332bc14f2e288ace9f7d1c690fb1a0d9d
michalliss <michal.j.liss@gmail.com>
2023-06-10 20:22:52 +0200

    clean dockerfile, better example

--- docker/master_volume/spark_scripts/test.py
+++ docker/master_volume/spark_scripts/test.py
@@ -4,7 +4,9 @@ covid = "/datasets/covid-dataset.jsonl"
 spark = SparkSession.builder.appName("CovidApp").getOrCreate()
 
 df = spark.read.json(covid)
+df.select("location").write.csv('/spark-result/dataframe-select', header=True)
 
-df.select("location").write.csv('/spark-result/foo.csv', header=True)
+spark.read.json(covid).createOrReplaceTempView("covid")
+spark.sql("SELECT location FROM covid").write.csv('/spark-result/sql-select', header=True)
 
 spark.stop()
