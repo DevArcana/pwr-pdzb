0a4aecf18e0d141dec53989466d57ad589d80156
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2023-06-17 18:08:03 +0200

    implement steam draft in spark

--- docker/master_volume/spark_scripts/steam_sql.py
+++ docker/master_volume/spark_scripts/steam_sql.py
@@ -1,18 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-steam = "/datasets/steam-dataset/steam_dataset/appinfo/store_data/steam_store_data.jsonl"
+steam_store = "/datasets/steam-dataset/steam_dataset/appinfo/store_data/steam_store_data.jsonl"
+steam_spy   = "/datasets/steam-dataset/steam_dataset/steamspy/basic/steam_spy_scrap.jsonl"
+
 spark = SparkSession.builder.appName("SteamApp").getOrCreate()
 
-spark.read.json(steam).select(
-    col("value.type").alias("type"),
-    col("value.name").alias("type"),
-    col("value.steam_appid").alias("type"),
-    col("value.type").alias("type"),
-    col("value.type").alias("type")
-).createOrReplaceTempView("steam")
+spark.read.json(steam_store).select(
+    col("value.steam_appid").alias("steam_appid"),
+    col("value.release_date.coming_soon").alias("coming_soon"),
+    col("value.release_date.date").alias("date")
+).createOrReplaceTempView("steam_store")
+
+spark.read.json(steam_spy).select(
+    col("value.appid").alias("appid"),
+    col("value.name").alias("name"),
+    col("value.positive").alias("positive"),
+    col("value.negative").alias("negative"),
+    col("value.owners").alias("owners"),
+    col("value.ccu").alias("ccu")
+).createOrReplaceTempView("steam_spy")
+
 spark.sql("""
-SELECT * FROM steam
+SELECT * from steam_spy
 """).write.csv('/spark-result/steam/sql', header=True)
 
 spark.stop()
