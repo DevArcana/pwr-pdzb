42f1b72577789fad762b1834160a5b7cd021981b
michalliss <michal.j.liss@gmail.com>
2023-06-20 00:24:02 +0200

    Working yey

--- docker/sprawozdania/utils.py
+++ docker/sprawozdania/utils.py
@@ -14,6 +14,18 @@ def run_in_hive(command: str, username: str = "root", password:str="pass"):
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(f"bash -c '. /env_var_path.sh && {command}'")
     return (ssh_stdout.readlines(), ssh_stderr.readlines())
 
-def print_hdfs_output(path, max_lines:int = 1000):
-    raw = run_in_master(f"hdfs dfs -cat {get_data_from_output_path(path)}")[0]
+def print_hdfs_output(path, max_lines:int = 10):
+    raw = run_in_master(f"hdfs dfs -cat {path}")[0]
     print("\n".join(raw[0:max_lines]))
+
+def hdfs_mkdir(path):
+    run_in_master(f"hdfs dfs -mkdir -p /{path}/")
+
+def hdfs_upload(path):
+    directory = "/".join(path.split("/")[:-1])
+    hdfs_mkdir(directory)
+    cmd = f"hdfs dfs -put /data/master_volume/{path} /{directory}"
+    print(cmd)
+    code, output = run_in_master(cmd)
+    print(f"exit code {code}")
+    print(output)
